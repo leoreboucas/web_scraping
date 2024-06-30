4d5fa8eab9f30e189b3667a8e4b5d12014162f2f
Leonardo Rebouças <leoabcblm@gmail.com>
2024-06-30 15:22:23 -0300

    docs: Add documentation for each step of the algorithm

--- home/utils.py
+++ home/utils.py
@@ -1,16 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+# O header 'User-Agent' é utilizado no programa para identificar o software cliente que está fazendo a requisição HTTP. Isso permite que o servidor saiba qual tipo de dispositivo ou aplicação está acessando seus recursos, facilitando a adaptação da resposta conforme necessário.
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.124 Safari/537.36"
 }
 
+# Função responsável pela raspagem de dados do site.
 def news_access(url):
+    
+    # A variável "r" é usada para fazer a requisição HTTP, enquanto a variável "soup" utiliza o BeautifulSoup para organizar e analisar a resposta dessa requisição.
     r = requests.get(f"{url}", headers=headers)
 
-    site = BeautifulSoup(r.text, "html.parser")
+    soup = BeautifulSoup(r.text, "html.parser")
 
-    search_notices = site.find("ul", {
+    # Essa parte do código é a parte principal que é pegar os dados na página e transformar em uma dict(dicionário) que contém o título da notícia que direciona para o link original e o conteúdo da notícia
+    search_notices = soup.find("ul", {
         "data-test":"news-list"
     })
 
@@ -25,5 +30,3 @@ def news_access(url):
         text = title.p.get_text()
         notices_titles[id] = {'link': link, 'text': text}
     return notices_titles
-
-news_access('https://www.investing.com/news/latest-news')
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .utils import news_access
 
+
+# Essa view é a que cria o template para o index.html, nele contém a variável notices que pega as notícias da função news_acess do arquivo utils.py e conecta ao arquivo html
 URL = "https://www.investing.com/news/latest-news"
 
 def home(request):
